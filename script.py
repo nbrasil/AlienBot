8cbc470554f64cd88ccb914feabbecc6ff8cc50c
nb <nicollybrasil@hotmail.com>
2025-03-25 14:57:44 -0300

    finalizando o projeto

--- script.py
+++ script.py
@@ -21,9 +21,9 @@ class AlienBot:
     goodbye = "Okay, have a nice Earth day!"
 
     def __init__(self):
-        self.alienbabble = {'describe_planet_intent': r'',
-                            'answer_why_intent': r'',
-                            'cubed_intent': r''
+        self.alienbabble = {'describe_planet_intent': r'(?i)(?:.*\b(?:your|the)\s+planet\b.*)',
+                            'answer_why_intent': r'(?i)(why\s+are\s+you\s+here|what\s+brings\s+you\s+to\s+earth|why\b.*)',
+                            'cubed_intent': r'(?i)(?:.*\bcube\b.*?(\d+))'
                             }
 
     # Define .greet() below:
@@ -38,20 +38,54 @@ class AlienBot:
     # Define .make_exit() here:
     def make_exit(self, reply):
         for word in self.exit_commands:
-            if reply in word:
+            if word in reply:
                 print(self.goodbye)
                 return True
+        return False
 
     # Define .chat() next:
     def chat(self):
         reply = input(random.choice(self.random_questions)).lower()
         while not self.make_exit(reply):
-            reply = input("How are you? ")
+            reply = input(self.match_reply(reply))
 
     # Define .match_reply() below:
     def match_reply(self, reply):
-        pass
+        for key, value in self.alienbabble.items():
+            found_match = re.search(value, reply)
+            if found_match:
+                if key == "describe_planet_intent":
+                    responses = (
+                        "My planet is a utopia of diverse organisms and species. ",
+                        "I am from Opidipus, the capital of the Wayward Galaxies. "
+                    )
+                    return random.choice(responses)
+                
+                elif key == "answer_why_intent":
+                    responses = (
+                        "I come in peace. ",
+                        "I am here to collect data on your planet and its inhabitants. ",
+                        "I heard the coffee is good. "
+                    )
+                    return random.choice(responses)
+                
+                elif key == "cubed_intent":
+                    number = int(found_match.group(1))
+                    cubed_number = number ** 3
+                    return f"The cube of {number} is {cubed_number}. Isn't that cool? "
+        
 
+        responses = (
+            "Please tell me more. ", "Tell me more! ",
+            "Why do you say that? ",
+            "I see. Can you elaborate? ",
+            "Interesting. Can you tell me more? ",
+            "I see. How do you think? ",
+            "Why? ",
+            "How do you think I feel when you say that? "
+        )
+        return random.choice(responses)
+    
     # Define .describe_planet_intent():
     def describe_planet_intent(self):
         return "Inside .describe_planet_intent()"
